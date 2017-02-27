3a223a98779da82cfcda53f6e09699c2990bf597
tevtonez <tevtonez@gmail.com>
2017-02-27 19:50:51 +0100

    create initial set of sub-tasks for task 12

---
+++ 12_work_with_pats.py
@@ -0,0 +1,46 @@
+'''
+Created on Feb 27, 2017
+@author: kostya
+
+TASK(s)
+
+1. print ful path of your working directory.
+2. write a script that creates a directory 'test1' in your working directory.
+3. write a script that creates a sub-directory in 'test1'. Name it 'test2'.
+4. write a script that creates 5 files in sub-directory 'test2'. Name files test-2-[n].txt , where [n] is the number from 1 to 5.
+5. write a script that gets the number of items in 'test1' directory.
+6. write a script that iterates through files only and changes their names to test-[n].txt , where [n] is the number from 1 to 5.
+
+'''
+
+if __name__ == '__main__':
+
+
+
+
+#===============================================================================
+# EXECUTION
+#===============================================================================
+
+    pass
+
+
+
+
+#===============================================================================
+# time spent: xx min
+#===============================================================================
+
+
+# Copyright (c) 2017, Konstantin Chernukhin, All rights reserved.
+# Created as a part of learning and practicing process.
+#
+# Author's url: http://octogear.com
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# IABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
